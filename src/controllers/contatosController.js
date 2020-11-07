b62d63194dbf96bef6f6b9523ad807ad56ed9d5f
Luana-Leticia <luanaleticiasouza@hotmail.com>
2020-11-07 15:50:35 -0300

    Criação dos métodos GET por id e POST novo contato

--- src/controllers/contatosController.js
+++ src/controllers/contatosController.js
@@ -5,14 +5,73 @@ const getAll = (request, response) => {
         if (error) {
             response.status(500).send(error);
         } else {
-            response.status(200).send({ 
-                message: "Mensagem temporária de ok",
+            response.status(200).json({ 
+                mensagem: "GET com sucesso",
                 contatos: contatos
         });
         }
     });
 }
 
+const getByName = (request, response) => {
+    
+}
+
+const getById = (request, response) => {
+    const contatoId = request.params.id;
+    contatoCollections.findById(contatoId, (error, contato) => {
+        if (error) {
+            response.status(500).send(error);
+        } else if (contato) {
+            response.status(200).send({
+                mensagem: "GET com sucesso",
+                contato: contato
+            });
+        } else {
+            response.status(404).json({
+                mensagem: "Contato não encontrado"
+            });
+        }
+    })
+
+}
+
+const addContato = (request, response) => {
+    const novoContato = request.body;
+    const contatoAdicionado = new contatoCollections(novoContato);
+    contatoAdicionado.save((error) => {
+        if (error) {
+            response.status(400).send(error);
+        } else {
+            response.status(200).json({
+                mensagem: "POST com sucesso",
+                contato: contatoAdicionado
+            })
+        }
+    })
+
+}
+
+const deleteContato = (request, response) => {
+    const contatoId = request.params.id;
+    contatoCollections.findByIdAndDelete(contatoId, (error, contato) => {
+        if (error) {
+            response.status(500).send(error);
+        } else if (contato) {
+            response.status(200).json({
+                mensagem: "DELETE com sucesso"
+            });
+        } else {
+            response.status(404).json({ mensagem: "Contato não encontrado"});
+        }
+    });
+}
+
+
 module.exports = {
-    getAll
+    getAll,
+    getByName,
+    getById,
+    addContato,
+    deleteContato
 }
--- src/routes/contatosRoute.js
+++ src/routes/contatosRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/contatosController')
+const controller = require('../controllers/contatosController');
 
 // rotas e documentações
 
@@ -8,6 +8,26 @@ const controller = require('../controllers/contatosController')
 //@desc Retornar todos os contatos
 //@acess Public
 //@endpoint http://localhost:porta/contatos
-router.get('/', controller.getAll)
+router.get('/', controller.getAll);
+
+//@route GET contato
+//@params :nome
+//@desc Retornar apenas um único contato pelo nome
+//@access Public
+//@endpoint http://localhost:porta/contatos/nome/:nome
+//router.get('/nome/:nome', controller.getByName);
+
+//@route GET contato
+//@params :id
+//@desc Retornar apenas um único contato pelo id : identificador
+//@access Public
+//@endpoint http://localhost:porta/contatos/id/:id
+router.get('/id/:id', controller.getById);
+
+//@route POST contato
+//@desc Adicionar um contato
+//@acess Public
+//@endpoint http://localhost:porta/contatos/criar
+router.post('/criar', controller.addContato);
 
 module.exports = router;
